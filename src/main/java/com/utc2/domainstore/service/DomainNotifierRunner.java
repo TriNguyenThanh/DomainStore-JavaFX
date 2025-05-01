a93325e62354435b260484deae3a334e29bf3c9b
Âu Dương Tấn <auduongtan321@gmail.com>
2025-05-01 14:46:57 +0700

    Fix de email tu dong gui khi domain sap het han

---
+++ src/main/java/com/utc2/domainstore/service/DomainNotifierRunner.java
@@ -0,0 +1,27 @@
+package com.utc2.domainstore.service;
+
+import com.utc2.domainstore.service.DomainNotifierServices;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class DomainNotifierRunner {
+    public static void main(String[] args) {
+        DomainNotifierServices notifierService = new DomainNotifierServices();
+
+        // Lấy danh sách domain sắp hết hạn từ CSDL
+        JSONObject expiringDomainsResponse = notifierService.getExpiringDomainsAsJson();
+
+        if (expiringDomainsResponse.getString("status").equals("success")) {
+            JSONArray domainArray = expiringDomainsResponse.getJSONArray("domains");
+
+            // Gửi email thông báo đến các người dùng
+            JSONObject notifyResult = notifierService.notifyExpiringDomains(domainArray);
+
+            System.out.println("Kết quả gửi email:");
+            System.out.println(notifyResult.toString(2));
+        } else {
+            System.err.println("Lỗi khi lấy dữ liệu domain: " +
+                    expiringDomainsResponse.getString("message"));
+        }
+    }
+}
