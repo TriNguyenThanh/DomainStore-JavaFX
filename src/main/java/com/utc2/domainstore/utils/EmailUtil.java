55916e9f3805635866c357b907b3e494242f96f2
Lê Nguyễn Anh Dự <dule1028a@gmail.com>
2025-05-02 15:31:07 +0700

    Update gửi file qua email

--- src/main/java/com/utc2/domainstore/utils/EmailUtil.java
+++ src/main/java/com/utc2/domainstore/utils/EmailUtil.java
@@ -2,6 +2,9 @@ package com.utc2.domainstore.utils;
 
 import jakarta.mail.*;
 import jakarta.mail.internet.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 public class EmailUtil {
@@ -32,6 +35,47 @@ public class EmailUtil {
             System.out.println("Email sent to: " + to);
         } catch (MessagingException e) {
             e.printStackTrace();
+        }
+    }
+    public static void sendEmailFile(String to, String subject, String content, String pathFile) {
+        final String from = "domainmanagementdtt@gmail.com";
+        final String password = "nknr ukbk blam berk"; // App password từ Gmail
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        try {
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(from));
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            msg.setSubject(subject);
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText(content);
+
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            attachmentPart.attachFile(new File(pathFile));
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(textPart);
+            multipart.addBodyPart(attachmentPart);
+
+            msg.setContent(multipart);
+
+            Transport.send(msg);
+            System.out.println("Email sent to: " + to);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
